51216d9f2a9fbaf3e16136e3de003da8c75f01d0
Tron <tron@tron-X401A1.(none)>
2013-02-28 14:12:35 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pokem0n::Application.config.secret_token = 'a261f26f4e689c1a000cf3173c95404c648a86508e715050db097f6b36c5c7e2093fd2cfd79a9e40bcfcbad60385379495b203345064b6693cf9871626ea6141'
