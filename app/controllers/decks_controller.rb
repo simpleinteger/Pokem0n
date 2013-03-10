4102a07cf8d780f0e9fa6eec3f8dfd45164e8fd4
Tron <tron@tron-X401A1.(none)>
2013-03-10 11:53:14 -0700

    added notification and redirect when completeing playing a deck, also added notification and redirect  when there are no cards in a deck

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -76,17 +76,25 @@ class DecksController < ApplicationController
     @current_deck = Deck.find(params[:deck_id])
     @deck_time = @current_deck.time
     @past_card_time = params[:time_id]
+
     if (@past_card_time.to_i == @deck_time)
       @text = 'deck_time' 
-      @current_card = @current_deck.cards.first
+
+      if (@current_card = @current_deck.cards.first).nil?
+        respond_to do |format|
+          format.html { redirect_to decks_path, notice: 'Deck does not have any cards'}
+          format.json { head :no_content }
+        end
+      end
+
+
     else 
       @text = 'card_time'
       #get the next card based on the next biggest card time.
-      @current_card = @current_deck.cards.where(:time.gt => @past_card_time).first
-        #if there are no more cards redirect to decks
-        if @current_card.nil?
+      if (@current_card = @current_deck.cards.where(:time.gt => @past_card_time).first).nil?
+        #if there are no more cards redirect to decks indexs and display message
            respond_to do |format|
-             format.html { redirect_to decks_path }
+             format.html { redirect_to decks_path, notice: 'You have completed the deck'}
              format.json { head :no_content }
             end
         end
